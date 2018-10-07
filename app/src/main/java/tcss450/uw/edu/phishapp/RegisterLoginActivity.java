3c7a83b7941ac2b9940a78d385a97afad8fa91fc
Josh Atherton <joshua9@uw.edu>
2018-10-07 08:42:19 -0700

    Hooked first login UI to action

--- app/src/main/java/tcss450/uw/edu/phishapp/RegisterLoginActivity.java
+++ app/src/main/java/tcss450/uw/edu/phishapp/RegisterLoginActivity.java
@@ -2,12 +2,49 @@ package tcss450.uw.edu.phishapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class RegisterLoginActivity extends AppCompatActivity {
+public class RegisterLoginActivity extends AppCompatActivity
+        implements UserLoginFragment.OnFragmentUserLoginInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_login);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.register_login_container) != null) {
+            Log.d("RegisterLoginActivity", "in the first if statement");
+
+            // If being restored from a previous state we don't need to do anything
+            // to avoid overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+            Log.d("RegisterLoginActivity", "past second if statement");
+            // Create a new Fragment to be placed in the activity layout
+            UserLoginFragment ulFragment = new UserLoginFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            ulFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.register_login_container, ulFragment).commit();
+        }
+    }
+
+
+    @Override
+    public void onFragmentInteractionLoginClicked() {
+
+    }
+
+    @Override
+    public void onFragmentInteractionRegisterClicked() {
+
     }
 }
+
--- app/src/main/java/tcss450/uw/edu/phishapp/UserLoginFragment.java
+++ app/src/main/java/tcss450/uw/edu/phishapp/UserLoginFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +13,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link UserLoginFragment.OnFragmentInteractionListener} interface
+
  * to handle interaction events.
  */
 public class UserLoginFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    private OnFragmentUserLoginInteractionListener mListener;
 
     public UserLoginFragment() {
         // Required empty public constructor
@@ -28,21 +29,17 @@ public class UserLoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_login, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_user_login, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        Log.d("UserLoginFragment", "inflating the fragment");
+        return v;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnFragmentUserLoginInteractionListener) {
+            mListener = (OnFragmentUserLoginInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -65,8 +62,9 @@ public class UserLoginFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface OnFragmentUserLoginInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onFragmentInteractionLoginClicked();
+        void onFragmentInteractionRegisterClicked();
     }
 }
